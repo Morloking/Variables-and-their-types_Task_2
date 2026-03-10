927c9349b57dd730be71009ed23f955910379fb6
Morloking <dyubenkosergei@yandex.ru>
2026-03-11 00:52:09 +0300

    Update Sector.cpp

--- Sector.cpp
+++ Sector.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::cin;
-using std::string;
-
 int main() {
     setlocale(LC_ALL, "RUSSIAN");
-    cout << "Введите слово:\n";
-    string s{""};
-    cin >> s;
-    cout << "Вы ввели:\n";
-    cout << s << "\n";
+    std::cout << "Введите слово:\n";
+    std::string s{""};
+    std::cin >> s;
+    std::cout << "Вы ввели:\n";
+    std::cout << s << "\n";
 }
+
 
